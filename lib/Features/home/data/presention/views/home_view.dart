e2939c69787cc45df0fc7f6bad288c8736a76e86
Amar <mohamed.zewin27@gmail.com>
2023-09-15 08:42:47 +0300

    custom appbar

--- lib/Features/home/data/presention/views/home_view.dart
+++ lib/Features/home/data/presention/views/home_view.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookly/Features/home/data/presention/views/widegts/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return const SafeArea(
+      child: Scaffold(
+        body: HomeViewBody(),
 
+      ),
     );
   }
 }
