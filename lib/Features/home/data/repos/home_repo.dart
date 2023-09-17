bb9112949bc12993d75c2114c357ba33d50fae31
Amar <mohamed.zewin27@gmail.com>
2023-09-17 03:37:14 +0300

    create Failure

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_bookly/Features/home/data/models/BookModel.dart';
+import '../../../../core/errors/failures.dart';
 abstract class HomeRepo{
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+ Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+ Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
